554ad57037eed77fd41413cc15df363f032060a3
Wellington36 <wellington.71319@gmail.com>
2024-10-25 14:20:28 -0300

    Add part 2 of day 5

--- day5.lean
+++ day5.lean
@@ -20,8 +20,7 @@ def rule₁ (s : List Char) : Bool :=
 def rule₂ : List Char -> Bool
   | []        => false
   | [_]       => false
-  | c1 :: c2 :: rest => if c1 = c2 then true else rule₂ (c2 :: rest)
-
+  | c1 :: c2 :: cs => if c1 = c2 then true else rule₂ (c2 :: cs)
 
 def rule₃ (s : List Char) : Bool :=
   let forbidden := ['a', 'b'] :: ['c', 'd'] :: ['p', 'q'] :: ['x', 'y'] :: []
@@ -40,3 +39,34 @@ def count_nice_strings (ss : List String) : Nat :=
 
 
 -- Part 2:
+def pairs_of_chairs : List Char -> List (List Char)
+  | [] => [[]]
+  | [_] => [[]]
+  | c1 :: c2 :: cs => [c1, c2] :: (pairs_of_chairs <| c2 :: cs)
+
+def check_replication : List (List Char) -> Bool
+  | [] => false
+  | x :: xs => if x ∈ xs then true
+    else check_replication xs
+
+def rule₄ : List Char -> Bool :=
+  λ x => (check_replication (pairs_of_chairs x))
+
+def rule₅ : List Char -> Bool
+  | []        => false
+  | [_]       => false
+  | [_, _]    => false
+  | c1 :: c2 :: c3 :: rest => if c1 = c3 then true else rule₅ (c2 :: c3 :: rest)
+
+
+def is_nice₂ (s : String) : Bool :=
+  let chars := s.toList
+  rule₄ chars ∧ rule₅ chars
+
+def count_nice_strings₂ (ss : List String) : Nat :=
+  ss.foldl (λ acc s => if is_nice₂ s then acc + 1 else acc) 0
+
+
+
+
+#eval count_nice_strings₂ input_task
